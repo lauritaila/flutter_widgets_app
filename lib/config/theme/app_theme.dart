b17ddcd8460e1598325d5763aeb3d4e51081839c
Laura Avila <lavila@bigwise.com>
2025-02-12 11:10:45 -0400

    FEAT: add appbar theme

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -25,7 +25,7 @@ class AppTheme {
   ThemeData getTheme() => ThemeData(
       useMaterial3: true,
       colorSchemeSeed: colorList[selectedColor],
-      
+      appBarTheme: const AppBarTheme(centerTitle: false)
     );
 
 }
